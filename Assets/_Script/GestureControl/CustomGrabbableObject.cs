7a0517e2e7c9793dfeae03989399e3b5ab29d882
Eric Chiu <wf870125@gmail.com>
2017-04-24 01:51:40 -0500

    Log rotation working

--- Assets/_Script/GestureControl/CustomGrabbableObject.cs
+++ Assets/_Script/GestureControl/CustomGrabbableObject.cs
@@ -11,6 +11,8 @@ public class CustomGrabbableObject : GrabbableObject {
 	bool isHandExist;
 	bool isReleased;
 	Quaternion initAngle;
+	Vector3 initUp;
+	Vector3 initForward;
 
 	// Shader for Highlighting
 	Renderer[] childRenderers;
@@ -26,6 +28,8 @@ public class CustomGrabbableObject : GrabbableObject {
 		isHandExist = false;
 		isReleased = true;
 		initAngle = transform.rotation;
+		initUp = transform.up;
+		initForward = transform.forward;
 
 		childRenderers = GetComponentsInChildren<Renderer>();
 		initShaders = new List<Shader>();
@@ -96,12 +100,62 @@ public class CustomGrabbableObject : GrabbableObject {
 		newAngle.y = Mathf.Round(transform.eulerAngles.y/90) * 90;
 		newAngle.z = Mathf.Round(transform.eulerAngles.z/90) * 90;
 		transform.eulerAngles = newAngle;
+		float angle = Quaternion.Angle(initAngle, transform.rotation);
+		if (angle > 50) {
+			logRotationAxis();
+		}
 		initAngle = transform.rotation;
+		initUp = transform.up;
+		initForward = transform.forward;
 	}
 
 	bool checkRotationAngle() {
 		float delta = Quaternion.Angle(initAngle, transform.rotation);
 		return delta > 70;
+	}
+
+	void logRotationAxis() {
+		Vector3 crossUp = Vector3.Cross(initUp, transform.up);
+		if (crossUp.x > 0.5) {
+			Debug.Log("+x");
+			SimpleData.WriteDataPoint("Rotate_Object", gameObject.name, "", "", "", "X");
+		} else if (crossUp.x < -0.5) {
+			Debug.Log("-x");
+			SimpleData.WriteDataPoint("Rotate_Object", gameObject.name, "", "", "", "X");
+		} else if (crossUp.y > 0.5) {
+			Debug.Log("+y");
+			SimpleData.WriteDataPoint("Rotate_Object", gameObject.name, "", "", "", "Y");
+		} else if (crossUp.y < -0.5) {
+			Debug.Log("-y");
+			SimpleData.WriteDataPoint("Rotate_Object", gameObject.name, "", "", "", "Y");
+		} else if (crossUp.z > 0.5) {
+			Debug.Log("+z");
+			SimpleData.WriteDataPoint("Rotate_Object", gameObject.name, "", "", "", "Z");
+		} else if (crossUp.z < -0.5) {
+			Debug.Log("-z");
+			SimpleData.WriteDataPoint("Rotate_Object", gameObject.name, "", "", "", "Z");
+		} else {
+			Vector3 crossForward = Vector3.Cross(initForward, transform.forward);
+			if (crossForward.x > 0.5) {
+				Debug.Log("+x");
+				SimpleData.WriteDataPoint("Rotate_Object", gameObject.name, "", "", "", "X");
+			} else if (crossForward.x < -0.5) {
+				Debug.Log("-x");
+				SimpleData.WriteDataPoint("Rotate_Object", gameObject.name, "", "", "", "X");
+			} else if (crossForward.y > 0.5) {
+				Debug.Log("+y");
+				SimpleData.WriteDataPoint("Rotate_Object", gameObject.name, "", "", "", "Y");
+			} else if (crossForward.y < -0.5) {
+				Debug.Log("-y");
+				SimpleData.WriteDataPoint("Rotate_Object", gameObject.name, "", "", "", "Y");
+			} else if (crossForward.z > 0.5) {
+				Debug.Log("+z");
+				SimpleData.WriteDataPoint("Rotate_Object", gameObject.name, "", "", "", "Z");
+			} else if (crossForward.z < -0.5) {
+				Debug.Log("-z");
+				SimpleData.WriteDataPoint("Rotate_Object", gameObject.name, "", "", "", "Z");
+			}
+		}
 	}
 
 	void highlightChildren() {
