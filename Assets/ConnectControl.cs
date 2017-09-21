e31452442f552dce986526368a694aee3c039901
Eric Chiu <wf870125@gmail.com>
2017-09-21 15:38:40 -0500

    Keep hand flat to enable moving/rotating from any distance

---
+++ Assets/ConnectControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectControl : MonoBehaviour {
+	SelectPart sp;
+
+	// Use this for initialization
+	void Start () {
+		sp = GetComponent<SelectPart>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		GameObject selectedObject = sp.getSelectedObject();
+		GameObject selectedFuseTo = sp.getSelectedFuseTo();
+		if (selectedObject != null && selectedFuseTo != null) {
+
+		}
+	}
+}
---
+++ Assets/ShowConnect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowConnect : MonoBehaviour {
+	SelectPart sp;
+	// Use this for initialization
+	void Start () {
+		sp = GetComponent<SelectPart>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		GameObject selectedObject = sp.getSelectedObject();
+		GameObject selectedFuseTo = sp.getSelectedFuseTo();
+		if (selectedObject != null && selectedFuseTo != null) {
+			// TODO
+		}
+	}
+}
